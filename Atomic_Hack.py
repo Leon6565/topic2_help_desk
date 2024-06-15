6f6fd9b52efb54618f8ae17f0cfaa56b276e6bfe
Квас Евгений <E.Kvas@innotech.team>
2024-06-16 00:44:49 +0300

    first commit

---
+++ Atomic_Hack.py
@@ -0,0 +1,66 @@
+﻿from parser_pdf_to_exel import clean_text, extract_text_by_chapters, extract_pages_to_excel
+import os
+import pandas as pd
+import re
+def sanitize_filename(filename):
+    return "".join([c if c.isalnum() else "_" for c in filename])
+
+def process_pdfs_to_excel(input_folder, output_folder):
+    # Создаем выходную папку, если она не существует
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    
+    # Проходим по всем файлам в указанной папке
+    for filename in os.listdir(input_folder):
+        if filename.endswith('.pdf'):
+            pdf_path = os.path.join(input_folder, filename)
+            sanitized_filename = sanitize_filename(os.path.splitext(filename)[0])
+            output_file = os.path.join(output_folder, f"{sanitized_filename}.xlsx")
+            print(filename)
+            try:
+                # Используем вашу функцию для извлечения текста
+                text_data = extract_text_by_chapters(pdf_path)
+                
+                # Создаем Excel файл используя вашу функцию
+                extract_pages_to_excel(text_data, output_file, input_filename = filename)
+            except Exception as e:
+                print(f"An error occurred while processing {filename}: {e}")
+
+# Пример использования функции
+input_folder = r'C:\Users\user\Desktop\Data_Science_study\_projects\Система технической поддержки\dataset\Для Хакатона'
+output_folder = r'C:\Users\user\Desktop\Data_Science_study\_projects\Система технической поддержки\dataset\excel'
+combined_output_file = r'C:\Users\user\Desktop\Data_Science_study\_projects\Система технической поддержки\dataset\excel\combined\combined_output_file.xlsx'
+#process_pdfs_to_excel(input_folder, output_folder)
+
+def combine_excel_files(output_folder, combined_output_file):
+    all_data = []
+    for filename in os.listdir(output_folder):
+        if filename.endswith('.xlsx'):
+            file_path = os.path.join(output_folder, filename)
+            df = pd.read_excel(file_path)
+            all_data.append(df)
+
+    combined_df = pd.concat(all_data, ignore_index=True)
+    combined_df.to_excel(combined_output_file, index=False)
+    
+#combine_excel_files(output_folder, combined_output_file)
+
+import pandas as pd
+import string
+
+# Загрузка Excel файла
+file_path = r'C:\Users\user\Desktop\Data_Science_study\_projects\Система технической поддержки\dataset\excel\combined\combined_output_file.xlsx'
+df = pd.read_excel(file_path)
+
+# Функция для удаления знаков препинания, тире, скобок и кавычек
+def remove_special_characters(text):
+    return re.sub(r'[^A-Za-zА-Яа-я0-9\s]', '', text)
+
+# Применение функции к третьему столбцу
+df.iloc[:, 2] = df.iloc[:, 2].apply(remove_special_characters)
+
+# Сохранение обработанного файла
+output_path = r'C:\Users\user\Desktop\Data_Science_study\_projects\Система технической поддержки\dataset\excel\combined\combined_output_file_clear.xlsx'
+df.to_excel(output_path, index=False)
+
+print("Знаки препинания удалены, файл сохранен как:", output_path)
---
+++ parser_pdf_to_exel.py
@@ -0,0 +1,118 @@
+﻿import pdfplumber
+import re
+import io
+import os
+import pandas as pd
+
+def clean_text(text):
+    # Регулярные выражения для удаления указанных надписей
+    patterns = [
+        r'АО «Гринатом»',  # АО «Гринатом»
+        r'Автор',  # Автор
+        r'Инструкция пользователей «[\s\S]*?»',  # Инструкция пользователей «<текст>»
+        r'Страниц\s+\d+',  # Страниц <число>
+        r'Версия\s+\d+',  # Версия <число>
+        r'Рисунок\s+\d+(:|\.|)'  # Рисунок <число>, Рисунок <число>:, Рисунок <число>.
+    ]
+    
+    for pattern in patterns:
+        text = re.sub(pattern, '', text, flags=re.DOTALL)
+    
+    return text.strip()
+
+def extract_text_by_chapters(pdf_path):
+    chapters = {}
+    current_chapter = None
+    current_subchapter = None
+
+    # Регулярные выражения для глав и подглав
+    chapter_pattern = re.compile(r'^\s*(\d+\.\d+)\s+(.*)$')
+    subchapter_pattern = re.compile(r'^\s*(\d+\.\d+\.\d+)\s+(.*)$')
+
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            text = page.extract_text()
+            if text:
+                lines = text.split('\n')
+                for line in lines:
+                    # Очистка линии от ненужных надписей
+                    cleaned_line = clean_text(line)
+                    
+                    chapter_match = chapter_pattern.match(cleaned_line)
+                    subchapter_match = subchapter_pattern.match(cleaned_line)
+
+                    if subchapter_match:
+                        current_subchapter = subchapter_match.group(1) + " " + subchapter_match.group(2)
+                        chapters[current_subchapter] = ""
+                    elif chapter_match:
+                        current_chapter = chapter_match.group(1) + " " + chapter_match.group(2)
+                        chapters[current_chapter] = ""
+                    elif current_subchapter:
+                        chapters[current_subchapter] += cleaned_line + "\n"
+                    elif current_chapter:
+                        chapters[current_chapter] += cleaned_line + "\n"
+    
+    # Удаление пустых строк и лишних пробелов
+    for key in chapters:
+        chapters[key] = chapters[key].strip()
+    # Создаем строковый буфер
+    output_buffer = io.StringIO()
+
+    # Записываем данные в буфер вместо вывода на экран
+    for key, value in chapters.items():
+        output_buffer.write(f"{key}:\n{value}\n{'-'*80}\n")
+
+    # Получаем строку из буфера
+    output_string = output_buffer.getvalue()
+
+    # Закрываем буфер
+    output_buffer.close()
+    return output_string
+
+# Пример использования функции
+#pdf_path = r'C:\Users\user\Desktop\Data_Science_study\_projects\Система технической поддержки\dataset\Инструкция_Учет_внутрихозяйственных_расчетов.pdf'
+#print(extract_text_by_chapters(pdf_path))
+
+
+def extract_pages_to_excel(text, output_file, output_path='.', input_filename='input.txt'):
+    try:
+        # Проверяем, существует ли директория и есть ли права на запись
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+        if not os.access(output_path, os.W_OK):
+            raise PermissionError(f"No write permission for the directory: {output_path}")
+
+        # Регулярное выражение для поиска страниц
+        pattern = re.compile(r'Страница (\d+)')
+        
+        # Разбиваем текст на части, используя регулярное выражение
+        parts = pattern.split(text)
+        
+        # Инициализируем список для хранения страниц
+        data = []
+        
+        # Перебираем части и извлекаем номер страницы и контент
+        for i in range(1, len(parts), 2):
+            page_number = int(parts[i])
+            content = parts[i + 1].strip().replace('\n', ' ')
+            data.append({
+                'Filename': input_filename,
+                'Page Number': page_number,
+                'Content': content
+            })
+        
+        # Создаем DataFrame
+        df = pd.DataFrame(data)
+
+        # Создаем полный путь к выходному файлу
+        full_output_path = os.path.join(output_path, output_file)
+        
+        # Записываем DataFrame в Excel-файл
+        df.to_excel(full_output_path, index=False)
+
+        print(f"File successfully saved to {full_output_path}")
+
+    except PermissionError as e:
+        print(e)
+    except Exception as e:
+        print(f"An error occurred: {e}")     
